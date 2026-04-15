c6549cfe4f598bdd53c97b9c24b74afcd588fe17
LERUO-M <leruomotlhammedev@gmail.com>
2026-04-15 17:34:10 +0200

    Replaced server with very basic server

--- contracts/LeftNFT.sol
+++ contracts/LeftNFT.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LeftNFT is ERC721, Ownable {
+contract LeftyNFT is ERC721, Ownable {
 
     uint256 public nextTokenId;
     string public baseURI; 
 
-    constructor() ERC721("Left NFT","LNFT") Ownable(msg.sender)
+    constructor() ERC721("Lefty NFT","LNFT") Ownable(msg.sender)
      {
         
     }
